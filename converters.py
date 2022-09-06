3ccb7e2909bf28cd36997100971fa43bc34317a7
Justin Sherman <jsherman2@gmail.com>
2022-09-05 19:16:57 -0700

    Add ACH (filtration) calculator

---
+++ converters.py
@@ -0,0 +1,8 @@
+def lps_to_cubic_meters_per_hour(lps):
+    return lps * 3.6
+
+def cfm_to_cubic_meters_per_hour(cfm):
+    return cfm * 1.699
+
+def cubic_feet_to_cubic_meters(cubic_feet):
+    return cubic_feet / 35.315
---
+++ pages/Air Changes - Filtration.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from converters import cfm_to_cubic_meters_per_hour, lps_to_cubic_meters_per_hour
+from lib import *
+from converters import *
+
+st.set_page_config(page_title='OSPE Air Quality Calculator', page_icon='💨')
+
+
+st.markdown("# OSPE Air Quality Calculator")
+st.markdown("## Air Changes from Filtration")
+st.markdown("Find the number of air changes per hour provided by an air purifier.")
+st.markdown("---")
+
+# Room parameters
+left, right = st.columns([2,1])
+with left:
+    max_cadr = st.number_input(label='Clear Air Delivery Rate (CADR)', step=1, min_value=1)
+with right:
+    cadr_unit = st.selectbox(label="CADR Unit", options=['CFM', 'lps', 'm^3/h'])
+
+left, right = st.columns([2,1])
+with left:
+    room_volume = st.number_input(label='Room Volume', step=1, min_value=1)
+with right:
+    volume_unit = st.selectbox(label='Volume Unit', options=['cubic feet', 'cubic meters'])
+
+# Convert CADR to cubic meters per hour (if necessary)
+if cadr_unit == 'm^3/h':
+    max_cadr_m3ph = max_cadr
+elif cadr_unit == 'CFM':
+    max_cadr_m3ph = cfm_to_cubic_meters_per_hour(max_cadr)
+elif cadr_unit == 'lps':
+    max_cadr_m3ph = lps_to_cubic_meters_per_hour(max_cadr)
+
+# Convert room volume to cubic meters
+if volume_unit == 'cubic meters':
+    volume_m3 = room_volume
+elif volume_unit == 'cubic feet':
+    volume_m3 = cubic_feet_to_cubic_meters(room_volume)
+
+# This *should* always be true, but best to double check
+if volume_m3 > 0:
+    ach = max_cadr_m3ph / volume_m3
+
+    # If ACH is within ±0.1 of an integer, just show the integer portion
+    # Otherwise, round to the nearest 0.1
+    ach = int(ach) if (abs(int(ach) - ach) < .1) else round(ach, 1)
+
+    st.write(f"# {ach} ACH")
